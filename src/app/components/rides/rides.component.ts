3e56ca4a7cde89d167dfb63756a978bd28b2d7ef
einarzb <einarzb@gmail.com>
2018-02-11 18:58:38 +0200

    simple get request to fetch all rides from api

--- src/app/components/rides/rides.component.ts
+++ src/app/components/rides/rides.component.ts
@@ -1,5 +1,6 @@
-import { Ride } from './../../models/Ride';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ride } from './../../models/Ride';
 
 @Component({
   selector: 'app-rides',
@@ -8,59 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RidesComponent implements OnInit {
-  rides: Ride[];
+  // rides: Ride[];
+  rides = '';
   stripColor:{};
   selectedColor:{};
 
-  constructor() { }
+  constructor(private http:HttpClient) {
 
-  ngOnInit() {
-    this.rides = [
-      {
-        id: 9,
-        zone: {
-        id: 2,
-        name: "Gibbon Island",
-        color: "#e76f68"
-        },
-        name: "Treetop Adventure",
-        remainingTickets: 30,
-        returnTime: new Date('01/02/2018 08:35:05')
-        },
-        {
-        id: 7,
-        zone: {
-        id: 2,
-        name: "Gibbon Island",
-        color: "#e76f68"
-        },
-        name: "Tropical Rush",
-        remainingTickets: 10,
-        returnTime: new Date('01/02/2018 08:35:05')
-        },
-        {
-        id: 8,
-        zone: {
-        id: 2,
-        name: "Gibbon Island",
-        color: "#e76f68"
-        },
-        name: "Rings of Black",
-        remainingTickets: 40,
-        returnTime: new Date('01/02/2018 08:35:05')
-        },
-        {
-        id: 6,
-        zone: {
-        id: 2,
-        name: "Gibbon Island",
-        color: "#e76f68"
-        },
-        name: "Full Moon Coaster",
-        remainingTickets: 5,
-        returnTime: new Date('01/02/2018 08:35:05')
-        }
-    ];
+   }
+
+  ngOnInit(): void {
+    this.http.get('http://fast-rider.herokuapp.com/api/v1/rides?token=d624351447c3d210a922fa846d3c13ce288b7a41cf').subscribe(data => {
+      console.log(data);
+    });
+  
+    // this.rides = [
+    //   {
+    //     id: 9,
+    //     zone: {
+    //     id: 2,
+    //     name: "Gibbon Island",
+    //     color: "#e76f68"
+    //     },
+    //     name: "Treetop Adventure",
+    //     remainingTickets: 30,
+    //     returnTime: new Date('01/02/2018 08:35:05')
+    //     },
+    //     {
+    //     id: 7,
+    //     zone: {
+    //     id: 2,
+    //     name: "Gibbon Island",
+    //     color: "#e76f68"
+    //     },
+    //     name: "Tropical Rush",
+    //     remainingTickets: 10,
+    //     returnTime: new Date('01/02/2018 08:35:05')
+    //     },
+    //     {
+    //     id: 8,
+    //     zone: {
+    //     id: 2,
+    //     name: "Gibbon Island",
+    //     color: "#e76f68"
+    //     },
+    //     name: "Rings of Black",
+    //     remainingTickets: 40,
+    //     returnTime: new Date('01/02/2018 08:35:05')
+    //     },
+    //     {
+    //     id: 6,
+    //     zone: {
+    //     id: 2,
+    //     name: "Gibbon Island",
+    //     color: "#e76f68"
+    //     },
+    //     name: "Full Moon Coaster",
+    //     remainingTickets: 5,
+    //     returnTime: new Date('01/02/2018 08:35:05')
+    //     }
+    // ];
     this.setColor();
   }
 
