a6c7ad7a0986d486e7bc01e6fb2627935d5baec3
einarzb <einarzb@gmail.com>
2018-02-11 18:52:02 +0200

    added httpclient module to module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { RidesComponent } from './components/rides/rides.component';
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
