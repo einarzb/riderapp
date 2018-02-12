d164276f144fdece83a2b5785901317b357146c4
einarzb <einarzb@gmail.com>
2018-02-12 16:05:42 +0200

    got most of the design

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ride } from './../../models/Ride';
 
 @Component({
   selector: 'app-ticket',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket.component.css']
 })
 export class TicketComponent implements OnInit {
+  rides: Ride[];
 
   constructor() { }
 
   ngOnInit() {
+    this.rides = [
+      {
+        id: 9,
+        zone: {
+        id: 2,
+        name: "Gibbon Island",
+        color: "#e76f68"
+        },
+        name: "Treetop Adventure",
+        remainingTickets: 30,
+        returnTime: new Date('01/02/2018 08:35:05')
+        }
+      ]
   }
 
 }
